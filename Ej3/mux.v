b82eea1afb59777bbc06a1af581026a25f2da2bf
Ezequiel <evijande@itba.edu.ar>
2018-08-20 16:57:44 -0300

    Termine el mux y su testbench, funcan

--- Ej3/mux.v
+++ Ej3/mux.v
@@ -1,28 +1,55 @@
-module mux(
-            select1,
-            select2,
-            input1,
-            input2,
-            input3,
-            input4,
-            out);
+/*
+Modulo que simula un mux de cuatro entradas y dos
+lineas de seleccion. Cuando solo la primera linea de
+seleccion esta prendida, correspone al segundo input. En cambio
+cuando solo la segunda linea esta prendida corresponde al
+tercer input.
+Asimismo, cuandos las dos estan apagadas corresponde al 
+primer input y todas prendidas al ultimo input.
+*/
+
+module mux(select1,select2,input1,input2,input3,input4, out);
 
         //Input ports
-        input input1, input2, input2, input3, input4;
+        input input1, input2, input3, input4;
         input select1, select2;
         //output ports
         output out;
-        wire a,b,c,d,e,f,g,h;
+        reg out;
 
-        if(select1 & select2)
-        begin
-          if(input1)
+        always @* begin
+
+          if(~(select1 || select2))
+          begin
+            if(input1)
+              out=1;
+              else
+              out=0;
+          end
+
+          else if((select2==0)&&(select1==1))
+          begin
+            if(input2)
             out=1;
-            else
-            out=0;
-        end
+              else
+              out=0;
+          end
+          else if((select2==1)&&(select1==0))
+          begin
+            if(input3)
+              out=1;
+              else
+              out=0;
+          end
+          else if(select2&&select1)
+          begin
+            if(input4)
+              out=1;
+              else
+              out=0;
+          end
 
-        else if()
+        end
 
 
 endmodule
