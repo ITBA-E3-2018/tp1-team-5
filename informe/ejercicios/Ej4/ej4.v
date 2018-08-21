5c096b93fd493afc50afe9876950191e21b0e126
Manuel Mollon <mmollon@itba.edu.ar>
2018-08-21 13:17:50 -0300

    Primer approach implementacion verilog ejercicio 4

---
+++ informe/ejercicios/Ej4/ej4.v
@@ -0,0 +1,29 @@
+module FourBitTwoComplement(
+    inputA,
+    inputB,
+    inputC,
+    inputD,
+    outputE,
+    outputF,
+    outputG,
+    outputH,
+);
+    input inputA;
+    input inputB;
+    input inputC;
+    input inputD;
+    output outputE;
+    output outputF;
+    output outputG;
+    output outputH;
+
+    assign outputH = inputD;
+    assign outputG = (inputC & ~inputD)|(~inputC & inputD);
+    assign outputF = (inputB & ~inputC & ~inputD)|(~inputB & inputC)|(~inputB & inputD);
+    assign outputC = (inputA & ~inputB & ~inputC & ~inputD)||(~inputA & ~inputC & inputD)||(~inputA & ~inputB & inputC)||(~inputA & inputB);
+endmodule
+
+module FourBitTwoComplement(
+);
+    
+endmodule
