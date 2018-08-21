c9ab8d42d3cef344ca4202f6eaa905e6336211d8
Manuel Mollon <mmollon@itba.edu.ar>
2018-08-21 13:36:59 -0300

    Siguo con el TB del ej 4

--- informe/ejercicios/Ej4/ej4.v
+++ informe/ejercicios/Ej4/ej4.v
@@ -23,7 +23,22 @@ module FourBitTwoComplement(
     assign outputC = (inputA & ~inputB & ~inputC & ~inputD)||(~inputA & ~inputC & inputD)||(~inputA & ~inputB & inputC)||(~inputA & inputB);
 endmodule
 
-module FourBitTwoComplement(
+module FourBitTwoComplementTB(
 );
-    
+    reg a,
+        b,
+        c,
+        d,
+        e,
+        f,
+        g,
+        h;
+    FourBitTwoComplement dut(a,b,d,c,e,f,g,h)
+
+    initial begin
+        a=0;b=0;c=0;d=0; #10;
+        a=1;b=0;c=0;d=0; #10;
+        a=1;b=1;c=0;d=0; #10;
+        a=0;b=1;c=1;d=1; #10;
+    end
 endmodule
