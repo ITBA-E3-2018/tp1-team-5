122891873381f37063916ab9f04ec5ff871c7c6f
Manuel Mollon <mmollon@itba.edu.ar>
2018-08-21 14:14:49 -0300

    Ya funciona verilog ej4 con TestBench de todos los casos posibles :)

--- informe/ejercicios/Ej4/ej4.v
+++ informe/ejercicios/Ej4/ej4.v
@@ -6,7 +6,7 @@ module FourBitTwoComplement(
     outputE,
     outputF,
     outputG,
-    outputH,
+    outputH
 );
     input inputA;
     input inputB;
@@ -20,7 +20,7 @@ module FourBitTwoComplement(
     assign outputH = inputD;
     assign outputG = (inputC & ~inputD)|(~inputC & inputD);
     assign outputF = (inputB & ~inputC & ~inputD)|(~inputB & inputC)|(~inputB & inputD);
-    assign outputC = (inputA & ~inputB & ~inputC & ~inputD)||(~inputA & ~inputC & inputD)||(~inputA & ~inputB & inputC)||(~inputA & inputB);
+    assign outputE = (inputA & ~inputB & ~inputC & ~inputD)||(~inputA & ~inputC & inputD)||(~inputA & ~inputB & inputC)||(~inputA & inputB);
 endmodule
 
 module FourBitTwoComplementTB(
@@ -28,17 +28,30 @@ module FourBitTwoComplementTB(
     reg a,
         b,
         c,
-        d,
-        e,
-        f,
-        g,
-        h;
-    FourBitTwoComplement dut(a,b,d,c,e,f,g,h)
-
+        d;
+    wire e,
+         f,
+         g,
+         h;
+    FourBitTwoComplement dut(a,b,c,d,e,f,g,h);
     initial begin
+        $monitor("Input:  a=%b   b=%b   c=%b   d=%b\nOutput: e=%b   f=%b   g=%b   h=%b",a,b,c,d,e,f,g,h);
         a=0;b=0;c=0;d=0; #10;
-        a=1;b=0;c=0;d=0; #10;
-        a=1;b=1;c=0;d=0; #10;
+        a=0;b=0;c=0;d=1; #10;
+        a=0;b=0;c=1;d=0; #10;
+        a=0;b=0;c=1;d=1; #10;
+        a=0;b=1;c=0;d=0; #10;
+        a=0;b=1;c=0;d=1; #10;
+        a=0;b=1;c=1;d=0; #10;
         a=0;b=1;c=1;d=1; #10;
+        a=1;b=0;c=0;d=0; #10;
+        a=1;b=0;c=0;d=1; #10;
+        a=1;b=0;c=1;d=0; #10;
+        a=1;b=0;c=1;d=1; #10;
+        a=1;b=1;c=0;d=0; #10;
+        a=1;b=1;c=0;d=1; #10;
+        a=1;b=1;c=1;d=0; #10;
+        a=1;b=1;c=1;d=1; #10;
+        $finish;
     end
 endmodule
